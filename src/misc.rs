5406df4a73527589c250cce6d797ede047957a59
Tools-app <localhost.hutao@gmail.com>
2026-04-11 06:43:20 +0800

    chore: fmt

--- src/misc.rs
+++ src/misc.rs
@@ -41,7 +41,10 @@ fn init_list() {
 }
 
 pub fn pre_init() {
-    assert!(!(std::env::var("KSU_LATE_LOAD").is_ok() && std::env::var("KSU").is_ok()), "! unsupported late load mode");
+    assert!(
+        !(std::env::var("KSU_LATE_LOAD").is_ok() && std::env::var("KSU").is_ok()),
+        "! unsupported late load mode"
+    );
     init_logger();
     init_list();
 }
